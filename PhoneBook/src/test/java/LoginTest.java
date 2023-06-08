6f8e41ccfc24b982f1117df079377042fd5efd36
Troianda <troiandisa@gmail.com>
2023-06-08 20:09:46 +0300

    8.06

--- PhoneBook/src/test/java/LoginTest.java
+++ PhoneBook/src/test/java/LoginTest.java
@@ -1,5 +1,20 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+    WebDriver wd;
     @BeforeMethod
-    public void setUp() throws Exception {
-    }
+        public void init() {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        }
+        @Test
+        public void testLogin() {
+
+        }
+        @AfterMethod
+    public void tearDown() {
+
+        }
 }
